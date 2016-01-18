8762f01c74325a891dbbe3084c43b12c12488b58
John Dzak <john.dzak@tablexi.com>
2016-01-17 22:10:10 -0600

    Show solutions in view

--- xkcd/ViewController.swift
+++ xkcd/ViewController.swift
@@ -23,8 +23,8 @@ struct ProblemCriteria {
   var menuItems: [MenuItem]
 
   static func fromFile(fileName: String) -> ProblemCriteria? {
-
-    guard let filePath = NSBundle.mainBundle().pathForResource(fileName, ofType: "json") else {
+    let componants = fileName.componentsSeparatedByString(".")
+    guard let filePath = NSBundle.mainBundle().pathForResource(componants[0], ofType: componants[1]) else {
       return nil
     }
 
@@ -38,18 +38,22 @@ struct ProblemCriteria {
   }
 
   static func fromJson(json: JSON) -> ProblemCriteria {
-    let targetPrice = NSDecimalNumber(decimal: json["target_price"].numberValue.decimalValue)
+    let targetPrice = NSDecimalNumber(string: self.removeDollarSign(json["target_price"].stringValue))
 
     var menuItems = [MenuItem]()
     for menuItemJson in json["items"].arrayValue {
       let dict = menuItemJson.dictionaryValue
-      if let name = dict["name"]?.stringValue, price = dict["price"]?.numberValue {
-        let price = NSDecimalNumber(decimal: price.decimalValue)
+      if let name = dict["name"]?.stringValue, priceStr = dict["price"]?.string {
+        let price = NSDecimalNumber(string: removeDollarSign(priceStr))
         menuItems.append(MenuItem(name: name, price: price))
       }
     }
 
     return ProblemCriteria(targetPrice: targetPrice, menuItems: menuItems)
+  }
+
+  static func removeDollarSign(price: String) -> String {
+    return price.stringByReplacingOccurrencesOfString("$", withString: "", options: NSStringCompareOptions.LiteralSearch, range: nil)
   }
 }
 
@@ -57,27 +61,36 @@ struct ProblemCriteria {
 
 class ViewController: UITableViewController {
   let CellIdentifier = "cell"
-  let stuff = ["foo", "bar", "baz"]
+  var solutions = [[MenuItem]]()
 
   override func viewDidLoad() {
     self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: CellIdentifier)
 
     if let criteria = ProblemCriteria.fromFile("menu.json") {
       var waiter = Waiter(menuItems: criteria.menuItems)
-      waiter.solve(criteria.targetPrice)
+      self.solutions = waiter.solve(criteria.targetPrice)
     }
   }
 
   override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
     let cell = self.tableView.dequeueReusableCellWithIdentifier(CellIdentifier, forIndexPath: indexPath)
 
-    cell.textLabel?.text = self.stuff[indexPath.row]
+    let section = self.solutions[indexPath.section]
+    cell.textLabel?.text = section[indexPath.row].name
 
     return cell
   }
 
+  override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    return self.solutions.count
+  }
+
   override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return self.stuff.count
+    return self.solutions[section].count
+  }
+
+  override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    return "Option \(section)"
   }
 }
 
